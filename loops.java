0c8ed8726868d8b9d317e197496566cc0699b971
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-01-24 22:06:58 +0300

    Update loops.java

--- loops.java
+++ loops.java
@@ -3,6 +3,7 @@ public class loops {
         for(int i=100;i>=1;i-=5){  //the i-=5 simply prints from 100 to 5 using muliples of 5.
             System.out.println(i);
         }
+
         System.out.println("Happy New  Month.");
     }
     
